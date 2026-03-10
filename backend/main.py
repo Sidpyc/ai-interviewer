aa747807c6a46c3aef661e5d614521132b3ff19e
Sidpyc <siddhufs2003@gmail.com>
2026-03-10 08:55:36 +0530

    changed  port variable in  main for backend

--- backend/main.py
+++ backend/main.py
@@ -1,3 +1,10 @@
+import os
+import uvicorn
+
 from src.api import create_app
 
 app = create_app()
+
+if __name__ == "__main__":
+    port = int(os.getenv("PORT", 8000))
+    uvicorn.run(app, host="0.0.0.0", port=port)
